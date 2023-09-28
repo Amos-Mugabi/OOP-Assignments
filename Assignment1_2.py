71070b6c4da2b032bd08beeb269356d1f8aec78b
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-09-28 14:41:37 +0300

    Add files via upload

---
+++ Assignment1_2.py
@@ -0,0 +1,13 @@
+
+import Assignment1
+
+# Using the utility functions
+result_sum = Assignment1.add_numbers(62, 8)
+result_product = Assignment1.multiply_numbers(7, 9)
+
+print(f"Sum: {result_sum}")
+print(f"Product: {result_product}")
+
+
+
+
